67ad3150ff1b3a2a262cde61d3e3195485b03d09
Jarno De Meyer <jarno.de.meyer@gmail.com>
2017-12-01 17:32:44 +0100

    Added master script for running multiple days

---
+++ allDays.pl
@@ -0,0 +1,9 @@
+@ARGV = (1..25) unless @ARGV;
+
+for (@ARGV){
+	my $filename = "day".($_<10?"0".$_:$_).".pl";
+	if(-e $filename){
+		printf "Day %2.2d\n------\n",$_;
+		system("perl $filename");
+	}
+}
